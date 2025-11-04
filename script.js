78fbb7e84076e831620638d631887665c817a0dc
oceanznx <rainer25santos@gmail.com>
2025-11-04 11:30:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+// Menu toggle
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+// Barra de pesquisa
+const searchInput = document.getElementById('searchInput');
+const clearBtn = document.getElementById('clearBtn');
+const jogos = document.querySelectorAll('.jogo');
+
+searchInput.addEventListener('input', () => {
+  const filter = searchInput.value.toLowerCase();
+  jogos.forEach(jogo => {
+    const title = jogo.querySelector('h4').textContent.toLowerCase();
+    if(title.includes(filter)){
+      jogo.style.display = '';
+    } else {
+      jogo.style.display = 'none';
+    }
+  });
+});
+
+clearBtn.addEventListener('click', () => {
+  searchInput.value = '';
+  jogos.forEach(jogo => jogo.style.display = '');
+});
